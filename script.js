a583f399f2bef06e4469eb4f0f4ddb56880820dd
Victor Volkov <vityan2504@gmail.com>
2021-06-05 12:36:47 +0300

    feat (script.js) создал функцию gameBot внутри startGame

--- script.js
+++ script.js
@@ -2,4 +2,44 @@
 
 const startGame = function() {
    let hiddenNumber = 95;
-}
+   
+
+   const gameBot = function () {
+      let userNumber = prompt("Угадай число от 1 до 100");
+
+      //if(userNumber < hiddenNumber) {
+      //   alert('Загаданное число меньше');
+      //   gameBot();
+      //} else if(userNumber > hiddenNumber) {
+      //   alert('Загаданное число больше');
+      //   gameBot();
+      //} else if(userNumber === null) {
+      //   alert('Игра окончена');
+      //   console.log(userNumber);
+      //} else if(isNaN(userNumber) || userNumber.trim() === '') {
+      //   alert('Введи число!');
+      //   gameBot();
+      //} else {
+      //   alert('Поздравляю, Вы угадали!!!');
+      //}
+
+      if(userNumber === null) {
+         alert('Игра окончена');
+      } else if(isNaN(userNumber) || userNumber.trim() === '') {
+         alert('Введи число!');
+         gameBot();
+      } else if(userNumber < hiddenNumber) {
+         alert('Загаданное число меньше');
+         gameBot();
+      } else if (userNumber > hiddenNumber) {
+         alert('Загаданное число больше');
+         gameBot();
+      } else {
+            alert('Поздравляю, Вы угадали!!!');
+      }
+   };
+
+   return gameBot();
+};
+
+startGame();
